3b09008a6b3e313e4b8d158747038221fbbe8ca8
StefanoMiceli90 <stefano.miceli@ext.leroymerlin.it>
2022-11-05 21:28:28 +0100

    Initial commit 2

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  // Authenticate the user
+  authenticateUser(username:string, password:string){
+    var body = {
+      "username": username,
+      "password": password
+    };
+    return this.httpClient.post(environment.base_url, body) ;
+  }
+
+
+}
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  saveTokenInStorage(token: string){
+    localStorage.setItem('token', token);
+  }
+
+  getTokenFromStorage(){
+    return localStorage.getItem('token');
+  }
+
+  removeTokenFromStorage(){
+    localStorage.removeItem('token');
+  }
+
+}
