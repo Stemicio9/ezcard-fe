538139234ad62483e220337e57c16977f782b637
StefanoMiceli90 <stefano.miceli@ext.leroymerlin.it>
2022-11-06 21:57:20 +0100

    some profile actions

---
+++ src/app/entities/profile.ts
@@ -0,0 +1,11 @@
+
+export class Profile {
+  username?: string;
+  name?: string;
+  surname?: string;
+  email?: string;
+  phone?: string;
+  address?: string;
+  city?: string;
+  country?: string;
+}
---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Profile} from "../entities/profile";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  insertProfile(profile: Profile){
+    return this.httpClient.post(environment.base_url + 'profile/insert', profile);
+  }
+
+  updateProfile(profile: Profile){
+    return this.httpClient.put(environment.base_url + 'profile/update', profile);
+  }
+
+  insertUser(username: string, password: string){
+    const body = {
+      "username": username,
+      "password": password
+    };
+    return this.httpClient.post(environment.base_url + 'profile/create', body);
+  }
+}
