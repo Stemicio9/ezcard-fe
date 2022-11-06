050b0f96d1ff02c5621963f89d441b71bda67e05
Stefano Miceli <s.miceli90@gmail.com>
2022-11-06 21:46:26 +0100

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {HttpClientModule} from "@angular/common/http";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
+    NgbModule,
     HttpClientModule
   ],
   providers: [],
