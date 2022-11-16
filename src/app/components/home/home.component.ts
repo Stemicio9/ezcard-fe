9e20b89e6d12e7aa56dfd90684fd3477a2b7fbed
SimoneBr <simonesuper555@gmail.com>
2022-11-16 13:31:03 +0100

    css refactored

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,10 +13,13 @@ import {CompanyModalComponent} from "../modal/company-modal/company-modal.compon
 export class HomeComponent implements OnInit {
 
   titlesFirstSection: string[] = ['Profilo', 'Contatti', 'Social', 'Azienda'];
+  titlesSecondSection: string[] = ['Statistiche', 'Impostazioni']
   descriptionsFirstSection: string[] = ['Nome, Compagnia, ecc.', 'Telefono, Mail, Sito web', 'Linkedin, Facebook, Instagram', 'Sito web, Telefono, Indirizzo'];
+  descriptionsSecondSection: string[] = ['Visite, Click, ecc.', 'Email, Lingua, ecc.'];
   imageLinksFirstSection: string[] = ["assets/user-icon.png","assets/email-icon.png","assets/share-icon.png","assets/azienda-icon.png"];
+  imageLinksSecondSection: string[] = ["assets/stats-icon.png","assets/settings-icon.png"];
   idModalList: any[] = [ProfileModalComponent, ContactsModalComponent, SocialModalComponent, CompanyModalComponent];
-
+  routerLinkList: string[] = ['/stats', '/settings'];
   closeResult = '';
 
   constructor(private modalService: NgbModal) { }
