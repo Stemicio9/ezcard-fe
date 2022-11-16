9dca72c4e0c8df95a534f1a806af317c84f5d08b
SimoneBr <simonesuper555@gmail.com>
2022-11-16 13:13:44 +0100

    popup refactoring

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {ProfileModalComponent} from "../modal/profile-modal/profile-modal.component";
+import {ContactsModalComponent} from "../modal/contacts-modal/contacts-modal.component";
+import {SocialModalComponent} from "../modal/social-modal/social-modal.component";
+import {CompanyModalComponent} from "../modal/company-modal/company-modal.component";
 
 @Component({
   selector: 'app-home',
@@ -9,11 +13,9 @@ import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class HomeComponent implements OnInit {
 
   titlesFirstSection: string[] = ['Profilo', 'Contatti', 'Social', 'Azienda'];
-  titlesSecondSection: string[] = ['Statistiche', 'Impostazioni', 'Password'];
   descriptionsFirstSection: string[] = ['Nome, Compagnia, ecc.', 'Telefono, Mail, Sito web', 'Linkedin, Facebook, Instagram', 'Sito web, Telefono, Indirizzo'];
-  descriptionsSecondSection: string[] = ['Visite, Click, ecc.','Email, Lingua, ecc.', 'Impostazioni password'];
   imageLinksFirstSection: string[] = ["assets/user-icon.png","assets/email-icon.png","assets/share-icon.png","assets/azienda-icon.png"];
-  imageLinksSecondSection: string[] = ["assets/stats-icon.png","assets/settings-icon.png","assets/password-icon.png"];
+  idModalList: any[] = [ProfileModalComponent, ContactsModalComponent, SocialModalComponent, CompanyModalComponent];
 
   closeResult = '';
 
