2782aa98ddedcab55137f94001690a69fa117d39
Stefano <s.miceli90@gmail.com>
2022-12-27 20:49:18 +0100

    reset password wip

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {StorageService} from "../../services/storage.service";
 import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
@@ -9,22 +10,53 @@ import {Router} from "@angular/router";
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
-
   form: any = {
     password: null,
     confirmPassword: null,
   };
+  disableButton = true;
+
+  confirmPasswordClass = 'form-control';
+  newPassword = new FormControl(null, [
+    (c: AbstractControl) => Validators.required(c),
+    Validators.pattern(
+      /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+    ),
+  ]);
+  confirmPassword = new FormControl(null, [
+    (c: AbstractControl) => Validators.required(c),
+    Validators.pattern(
+      /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+    ),
+  ]);
+
+  resetPasswordForm = this.formBuilder.group(
+    {
+      newPassword: this.newPassword,
+      confirmPassword: this.confirmPassword,
+    },
+    {
+      validator: this.ConfirmedValidator('newPassword', 'confirmPassword'),
+    }
+  );
 
   constructor(
     private storageService: StorageService,
     private authService: AuthService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   resetPassword() {
-    this.router.navigate(['home']);
+    if (!this.resetPasswordForm?.valid) {
+      return;
+    } else {
+      this.router.navigate(['home']);
+    }
 
     /*const { email, password } = this.form;
 
@@ -37,6 +69,27 @@ export class ResetPasswordComponent implements OnInit {
         console.log(error);
       }
     );*/
+  }
+
+  ConfirmedValidator(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+      if (
+        matchingControl.errors &&
+        !matchingControl.errors['confirmedValidator']
+      ) {
+        this.disableButton = true;
+        return;
+      }
+      if (control.value !== matchingControl.value) {
+        this.disableButton = true;
+        matchingControl.setErrors({confirmedValidator: true});
+      } else {
+        this.disableButton = false;
+        matchingControl.setErrors(null);
+      }
+    };
   }
 
 }
